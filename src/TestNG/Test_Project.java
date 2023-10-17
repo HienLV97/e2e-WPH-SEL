a41de6586aa7d5a86250a92e8c593231c7f6f0a0
Hienlv97 <levinhhien2603@gmail.com>
2023-10-18 00:06:22 +0700

    a

---
+++ src/TestNG/Test_Project.java
@@ -0,0 +1,31 @@
+package TestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Test_Project {
+    public WebDriver driver = null;
+
+    @Test
+    public void demo1() {
+        driver.navigate().to("https://anhtester.com");
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    }
+
+    @AfterTest
+    public void afterTest() throws InterruptedException {
+    Thread.sleep(2000);
+    }
+}
