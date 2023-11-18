3c3d7c4e9d7cf7e10e8e44ae6439795aec0635c1
HienLV97 <levinhhien2603@gmail.com>
2023-11-18 13:45:08 +0700

    up

--- src/MenthodAndClass/ActionsClass.java
+++ src/MenthodAndClass/ActionsClass.java
@@ -7,12 +7,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ActionsClass extends Init {
 
-    @Test//(enabled = false)
+    @Test(enabled = false)
     public void TestAction() {
 
         driver.get("https://www.google.com/");
@@ -34,7 +39,7 @@ public class ActionsClass extends Init {
         action.moveToElement(allCourseBTN).build().perform();
     }
 
-    @Test//(enabled = false)
+    @Test(enabled = false)
     public void DrapAndDrop() throws AWTException {
         Actions action = new Actions(driver);
         Robot robot = new Robot();
@@ -44,7 +49,7 @@ public class ActionsClass extends Init {
         action.dragAndDrop(From, To).build().perform();
     }
 
-    @Test(enabled = true)
+    @Test(enabled = false)
     public void Robot() throws AWTException {
         Actions action = new Actions(driver);
         driver.get("https://anhtester.com");
@@ -78,5 +83,47 @@ public class ActionsClass extends Init {
                 .keyUp("p")
                 .build().perform();
         sleep(2);
+    }
+    @Test(enabled = true)
+    public void mouseMove() throws AWTException {
+        Actions action = new Actions(driver);
+        driver.get("https://anhtester.com");
+
+        sleep(5);
+       Robot robot = new Robot();
+       robot.mouseMove(1440+2100,-300);
+       robot.delay(1000);
+       robot.mousePress(InputEvent.BUTTON2_DOWN_MASK);
+       sleep(1);
+       robot.mouseRelease(InputEvent.BUTTON2_DOWN_MASK);
+    }
+    @Test(enabled = true)
+    public void screenShot() throws AWTException, IOException {
+        Actions action = new Actions(driver);
+        driver.get("https://anhtester.com");
+
+//        sleep(5);
+        Robot robot = new Robot();
+        Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int XScreen2 = 1441 ;
+        int YScreen2 = 0 ;
+        int WidthScreen =100;
+        int HeightScreen = 100;
+        Rectangle screenRectangle = new Rectangle( XScreen2,YScreen2,WidthScreen,HeightScreen);
+//        Rectangle screenRectangle = new Rectangle();
+        BufferedImage image = robot.createScreenCapture(screenRectangle);
+        File file = new File("Test-"+XScreen2+"-"+YScreen2+"-"+WidthScreen+"-"+HeightScreen+".png");
+        ImageIO.write(image,"png",file);
+
+
+//        GraphicsDevice[] screens = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
+//        // In thông tin kích thước của từng màn hình
+//        for (int i = 0; i < screens.length; i++) {
+//            Dimension screenSize = screens[i].getDefaultConfiguration().getBounds().getSize();
+//            System.out.println("Screen " + (i + 1) + " Size: " + screenSize);
+//        }
+//        System.out.println(ScreenSize);
+
+
     }
 }
